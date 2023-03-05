2cbf19deed6c4bb3a0f55d8c2d19cc508c06a14f
seokamela <ntjobokoane@gmail.com>
2023-03-05 23:58:22 +0200

    vody

---
+++ body.js
@@ -0,0 +1,18 @@
+// const productImage = document.getElementById("product-image");
+// const thumbnailRow = document.getElementById("thumbnail-row");
+
+// // Array of image URLs
+// const images = [
+//   "images/image-product-1-thumbnail.jpg",
+//   "images/image-product-1-thumbnail.jpg",
+// ];
+
+// // Load product image
+// productImage.src = images[0];
+
+// // Load thumbnail images
+// for (let i = 1; i < images.length; i++) {
+//   const img = document.createElement("img");
+//   img.src = images[i];
+//   thumbnailRow.appendChild(img);
+// }
