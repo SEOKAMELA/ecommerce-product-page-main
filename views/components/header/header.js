f60d1ca6d2a1ea3fcddcebc53023379ea0efa199
seokamela <ntjobokoane@gmail.com>
2023-03-05 22:36:27 +0200

    header dynamic

--- views/components/header/header.js
+++ views/components/header/header.js
@@ -1,12 +1,27 @@
-const header = document.createElement('header');
-header.innerHTML = `
-  <h1>My Website</h1>
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
-`;
-document.body.insertBefore(header, document.body.firstChild);
+// fetch("header.html")
+// 	.then(response => response.text())
+// 	.then(data => {
+// 		document.getElementById("header").innerHTML = data;
+// 	});
+
+//   // Fetch CSS file and apply styles
+// fetch('header.css')
+// .then(response => response.text())
+// .then(data => {
+//   var style = document.createElement('style');
+//   style.textContent = data;
+//   document.head.appendChild(style);
+// })
+// .catch(error => console.log(error));
+
+
+// Fetch HTML file and replace the content of the body
+fetch('header.html')
+	.then(response => response.text())
+	.then(data => {
+		var parser = new DOMParser();
+		var htmlDoc = parser.parseFromString(data, 'text/html');
+		var header = htmlDoc.querySelector('.header');
+		document.body.replaceChild(header, document.querySelector('.header'));
+	})
+	.catch(error => console.log(error));
