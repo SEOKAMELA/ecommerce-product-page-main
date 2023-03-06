f790f2807392e471f2ceecc22f70a85afe8d09e9
seokamela <ntjobokoane@gmail.com>
2023-03-06 07:58:29 +0200

    vody

--- body.js
+++ body.js
@@ -1,18 +1,5 @@
-// const productImage = document.getElementById("product-image");
-// const thumbnailRow = document.getElementById("thumbnail-row");
+// Get a reference to the image element
+const productImage = document.getElementById('product-image');
 
-// // Array of image URLs
-// const images = [
-//   "images/image-product-1-thumbnail.jpg",
-//   "images/image-product-1-thumbnail.jpg",
-// ];
-
-// // Load product image
-// productImage.src = images[0];
-
-// // Load thumbnail images
-// for (let i = 1; i < images.length; i++) {
-//   const img = document.createElement("img");
-//   img.src = images[i];
-//   thumbnailRow.appendChild(img);
-// }
+// Set the src attribute to the URL of the image you want to display
+productImage.src = 'images/image-product-1.jpg';
--- script.js
+++ script.js
@@ -15,3 +15,10 @@ fetch('body.html')
 	.then(data => {
 		main.innerHTML = data;
 	});
+
+
+// Get a reference to the image element
+const productImage = document.getElementById('product-image');
+
+// Set the src attribute to the URL of the image you want to display
+productImage.src = 'images/image-product-1.jpg';
